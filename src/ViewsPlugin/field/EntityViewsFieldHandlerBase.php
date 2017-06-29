3359b602806a476c8a4cb5b8ea227243e263e22f
Andreas Hennings <andreas@dqxtech.net>
2017-06-29 20:37:23 +0200

    Fix EntityViewsFieldHandlerBase:
    - Make sure id field is in the query.
    - More sanity checks when getting result entities from query.
    - More messages if something goes wrong.

--- src/ViewsPlugin/field/EntityViewsFieldHandlerBase.php
+++ src/ViewsPlugin/field/EntityViewsFieldHandlerBase.php
@@ -19,6 +19,15 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
   private $fieldEntityType;
 
   /**
+   * Overrides @see \views_handler_field::$query, to declare other query types
+   * that implement an ->add_field() method, or where ->get_result_entities()
+   * has 3 parameters instead of 2.
+   *
+   * @var \views_plugin_query|\views_plugin_query_default|\SearchApiViewsQuery
+   */
+  var $query = NULL;
+
+  /**
    * Initializes the entity type with the field's entity type.
    *
    * @see \entity_views_handler_field_entity::init()
@@ -28,7 +37,9 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
    */
   public function init(&$view, &$options) {
     parent::init($view, $options);
+
     $fieldEntityType = EntityUtil::entityPropertyExtractInnermostType($this->definition['type']);
+
     if (!$fieldEntityType) {
       $message = 'Cannot determine field entity type.';
       watchdog('entdisp', $message);
@@ -36,6 +47,7 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
         drupal_set_message($message, 'warning');
       }
     }
+
     $this->fieldEntityType = $fieldEntityType;
     $this->initEntityType($fieldEntityType);
   }
@@ -45,8 +57,36 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
    */
   abstract protected function initEntityType($entityType);
 
+  /**
+   * Overriden to add the field for the entity id.
+   *
+   * @see \views_handler_field_entity::query()
+   */
   public function query() {
-    // do nothing -- to override the parent query.
+
+    $table_alias = $base_table = $this->view->base_table;
+    $base_field = $this->view->base_field;
+
+    if (!empty($this->relationship)) {
+      foreach ($this->view->relationship as $relationship) {
+        if ($relationship->alias == $this->relationship) {
+          $base_table = $relationship->definition['base'];
+          $table_alias = $relationship->alias;
+
+          $table_data = views_fetch_data($base_table);
+          $base_field = empty($relationship->definition['base field']) ? $table_data['table']['base']['field'] : $relationship->definition['base field'];
+        }
+      }
+    }
+
+    // Add the field if the query back-end implements an add_field() method,
+    // just like the default back-end.
+    if (method_exists($this->query, 'add_field')) {
+      $this->field_alias = $this->query->add_field($table_alias, $base_field, '');
+    }
+
+    // Additional fields are not needed.
+    # $this->add_additional_fields();
   }
 
   /**
@@ -59,27 +99,17 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
    *   An array of all objects returned from the query.
    */
   public function pre_render(&$rows) {
-    /**
-     * @var string $entityType
-     * @var object[] $entities
-     */
-    list($entityType, $entities) = $this->getResultEntities($rows);
-    if ($entityType !== $this->fieldEntityType) {
-      $message = 'Entity type mismatch.';
-      watchdog('entdisp', $message);
-      if (user_access('administer site configuration')) {
-        drupal_set_message($message, 'warning');
-      }
-    }
+
+    $entities = $this->getResultEntities($rows);
+
     // Build the entities.
-    $builds = $this->buildMultiple($entityType, $entities);
+    $builds = $this->buildMultiple($this->fieldEntityType, $entities);
+
     foreach ($rows as $rowIndex => $row) {
       if (isset($builds[$rowIndex])) {
-        /** @noinspection PhpUndefinedFieldInspection */
         $row->entdisp_field_builds[$this->position] = $builds[$rowIndex];
       }
       else {
-        /** @noinspection PhpUndefinedFieldInspection */
         unset($row->entdisp_field_builds[$this->position]);
       }
     }
@@ -88,17 +118,47 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
   /**
    * @param object[] $rows
    *
-   * @return mixed[]
+   * @return object[]
    *
    * @see EntityFieldHandlerHelper::pre_render()
    */
-  protected function getResultEntities(array $rows) {
+  private function getResultEntities(array $rows) {
+
     $relationship = !empty($this->relationship) ? $this->relationship : NULL;
-    $field_alias = isset($this->real_field) ? $this->real_field : NULL;
-    // Some views query classes want/allow a third parameter specifying the field name.
-    /** @noinspection PhpMethodParametersCountMismatchInspection */
-    list($entityType, $entities) = $this->view->query->get_result_entities($rows, $relationship, $field_alias);
-    return [$entityType, $entities];
+
+    $resultEntitiesOrFalse = $this->query->get_result_entities(
+      $rows,
+      $relationship,
+      // Some views query classes want/allow a third parameter specifying the
+      // field name.
+      $this->field_alias);
+
+    if (FALSE === $resultEntitiesOrFalse) {
+      $this->watchdogAndMessage(
+        'Cannot fetch entities: !method returned !false.',
+        [
+          '!method' => '<code>'
+            . get_class($this->query) . '::get_result_entities()'
+            . '</code>',
+          '!false' => '<code>FALSE</code>',
+        ]);
+
+      return [];
+    }
+
+    list($entityType, $entities) = $resultEntitiesOrFalse;
+
+    if ($entityType !== $this->fieldEntityType) {
+      $this->watchdogAndMessage(
+        'Entity type mismatch. Expected @expected, found @found instead.',
+        [
+          '@expected' => "'$entityType'",
+          '@found' => "'$this->fieldEntityType'",
+        ]);
+      return [];
+    }
+
+    return $entities;
   }
 
   /**
@@ -107,7 +167,6 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
    * @return string
    */
   public function render($row) {
-    /** @noinspection PhpUndefinedFieldInspection */
     return isset($row->entdisp_field_builds[$this->position])
       ? drupal_render($row->entdisp_field_builds[$this->position])
       : NULL;
@@ -121,5 +180,36 @@ abstract class EntityViewsFieldHandlerBase extends \views_handler_field {
    *   A render array for each entity.
    */
   abstract protected function buildMultiple($entityType, array $entities);
+
+  /**
+   * @param string $message
+   * @param string[] $replacements
+   */
+  private function watchdogAndMessage($message, array $replacements = []) {
+
+    $message .= "<br/>\n"
+      . 'In Views field %field_id of !views_display.';
+
+    $view = $this->view;
+    $view_name = $view->name;
+    $view_label = $view->get_human_name();
+    $display_id = $view->current_display;
+    $display = $view->display[$display_id];
+    $display_label = $display->display_title;
+
+    $replacements += [
+      '!views_display' => l(
+        "$view_label: $display_label",
+        "admin/structure/views/view/$view_name/edit/$display_id",
+        ['attributes' => ['target' => '_blank']]),
+      '%field_id' => $this->options['id'],
+    ];
+
+    watchdog('entdisp', $message, $replacements);
+
+    if (user_access('administer site configuration')) {
+      drupal_set_message(t($message, $replacements), 'warning');
+    }
+  }
 
 }
